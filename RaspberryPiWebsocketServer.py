8b6270709e0d5826b1ce2f55b44bc3751019cb69
zachcmathews <zachary.c.mathews@gmail.com>
2020-11-15 16:46:48 -0500

    rename SerialConnection to GPSStream; change serial connection parameters; change rate of writing

--- RaspberryPiWebsocketServer.py
+++ RaspberryPiWebsocketServer.py
@@ -8,13 +8,16 @@ import time
 import threading
 
 
-class SerialConnection:
+class GPSStream:
     """Manage the serial connection."""
 
     def __init__(self, port, data):
-        self.port = serial.Serial(port)
+        self.port = serial.Serial(port=port, 
+                                  baudrate=9600,
+                                  bytesize=serial.EIGHTBITS,
+                                  parity=serial.PARITY_NONE,
+                                  stopbits=serial.STOPBITS_ONE)
         self.data = data
-        self.msg = construct_gpgga(data)
         self.last_msg = None
         self.keep_alive()
 
@@ -24,19 +27,14 @@ class SerialConnection:
         Update the message that we're sending.
         """
         self.data = data
-        self.data['timestamp'] = datetime.now().timestamp()*1000
-        self.msg = construct_gpgga(data)
 
 
     def tick(self):
         """
-        Check that message has changed, 
-        otherwise update the timestamp
-        and reconstruct the sentence.
+        Update the message's timestamp and construct the sentence.
         """
-        if self.msg == self.last_msg:
-            self.data['timestamp'] = datetime.now().timestamp()*1000
-            self.msg = construct_gpgga(self.data)
+        self.data['timestamp'] = datetime.now().timestamp()*1000
+        self.msg = construct_gpgga(self.data)
 
 
     def keep_alive(self):
@@ -45,7 +43,7 @@ class SerialConnection:
         valid data.
         """
         self.tick()
-        threading.Timer(0.5, self.keep_alive).start()
+        threading.Timer(0.1, self.keep_alive).start()
         self.port.write(self.msg)
         self.last_msg = self.msg
         print(f'Sent: {self.msg}')
@@ -53,35 +51,51 @@ class SerialConnection:
 
 def construct_gpgga(data):
     """Construct an NMEA GPGGA sentence from raw data"""
-    utc = ms_to_utc(int(data['timestamp']))
-    lat = float(data['latitude'])
-    long = float(data['longitude'])
-    fix = 1
-    num_sats = 15
-    hdop = 1.5
-    altitude = float(data['altitude'])
-    dsep_geoid = 0
-    dgps_delta_t = ''
-    dgps_station = ''
+    fix = data.get('fix', 1)
 
-    if lat < 0:
-        lat_hemi = 'S'
-        lat = -lat
+    if fix:
+        utc = ms_to_utc(int(data['timestamp']))
+        lat = float(data['latitude'])
+        long = float(data['longitude'])
+        num_sats = 15
+        hdop = 1.5
+        altitude = float(data['altitude'])
+        dsep_geoid = 0
+        dgps_delta_t = ''
+        dgps_station = ''
+
+        if lat < 0:
+            lat_hemi = 'S'
+            lat = -lat
+        else:
+            lat_hemi = 'N'
+
+        if long < 0:
+            long_hemi = 'W'
+            long = -long
+        else:
+            long_hemi = 'E'
+
+        lat_degdmin = f'{int(lat):0>2d}{cvt_deg_to_dmin(lat-int(lat)):.4f}'
+        long_degdmin = f'{int(long):0>3d}{cvt_deg_to_dmin(long-int(long)):.4f}'
+
+        sentence = f'GPGGA,{utc},{lat_degdmin},{lat_hemi},{long_degdmin},{long_hemi},{fix},{num_sats},{hdop:.1f},{altitude:.1f},M,{dsep_geoid:.1f},M,{dgps_delta_t},{dgps_station}'
     else:
-        lat_hemi = 'N'
+        utc = ''
+        lat = ''
+        lat_hemi = ''
+        long = ''
+        long_hemi = ''
+        num_sats = '00'
+        hdop = '99.99'
+        altitude = ''
+        dsep_geoid = ''
+        dgps_delta_t = ''
+        dgps_station = ''
+        sentence = f'GPGGA,{utc},{lat},{lat_hemi},{long},{long_hemi},{fix},{num_sats},{hdop},{altitude},,{dsep_geoid},,{dgps_delta_t},{dgps_station}'
 
-    if long < 0:
-        long_hemi = 'W'
-        long = -long
-    else:
-        long_hemi = 'E'
-
-    lat_degdmin = f'{int(lat):0>2d}{cvt_deg_to_dmin(lat-int(lat)):.4f}'
-    long_degdmin = f'{int(long):0>3d}{cvt_deg_to_dmin(long-int(long)):.4f}'
-
-    sentence = f'GPGGA,{utc},{lat_degdmin},{lat_hemi},{long_degdmin},{long_hemi},{fix},{num_sats},{hdop:.1f},{altitude:.1f},M,{dsep_geoid:.1f},M,{dgps_delta_t},{dgps_station}'
     checksum = compute_checksum(sentence)
-    return f'${sentence}*{checksum:X}\r\n'.encode()
+    return f'${sentence}*{checksum:X}\r\n'.encode(encoding='ASCII', errors='strict')
 
 
 def compute_checksum(sentence):
@@ -95,7 +109,7 @@ def compute_checksum(sentence):
 
 def ms_to_utc(ms):
     """Convert POSIX timestamp to UTC time string"""
-    return datetime.utcfromtimestamp(ms/1000.0).strftime('%H%M%S')
+    return datetime.utcfromtimestamp(ms/1000.0).strftime('%H%M%S.%f')[:-3]
     
 
 def cvt_deg_to_dmin(deg):
@@ -103,10 +117,10 @@ def cvt_deg_to_dmin(deg):
     return deg * 60
 
 
-def on_message(ws, message, serial_connection):
+def on_message(ws, message, gps_stream):
     if "gpsdata: " in message:
         data = json.loads(message.lstrip("gpsdata: ").replace("\'", "\""))
-        serial_connection.send(data)
+        gps_stream.send(data)
 
 
 def on_error(ws, error):
@@ -135,15 +149,15 @@ def on_open(ws):
 
 
 if __name__ == "__main__":
-    mock_data = {'timestamp': datetime.now().timestamp()*1000, 'latitude': 0000.0000, 'longitude': 00000.0000, 'altitude': 0.0}
-    serial_connection = SerialConnection(port='/dev/serial0', data=mock_data)
-    print('Sending GPS data to serial port: ' + serial_connection.port.name) 
+    mock_data = { 'fix': 0 }
+    gps_stream = GPSStream(port='/dev/serial0', data=mock_data)
+    print('Sending GPS data to serial port: ' + gps_stream.port.name) 
     
     # For Debug:
     # websocket.enableTrace(True)
     print('Starting WebSocket connection to AWS.')
     ws = websocket.WebSocketApp("wss://10eoew3urf.execute-api.us-east-1.amazonaws.com/development",
-                                on_message=lambda ws, msg: on_message(ws, msg, serial_connection),
+                                on_message=lambda ws, msg: on_message(ws, msg, gps_stream),
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
