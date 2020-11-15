31acf92291ba4f36d145c4442e60856167dbba27
zachcmathews <zachary.c.mathews@gmail.com>
2020-11-15 13:41:32 -0500

    bunch of gps mocking stuff

--- RaspberryPiWebsocketServer.py
+++ RaspberryPiWebsocketServer.py
@@ -3,25 +3,110 @@ import websocket
 import json
 import serial
 from datetime import datetime
-
-try:
-    import thread
-except ImportError:
-    import _thread as thread
+import _thread as thread
 import time
+import threading
 
 
-def on_message(ws, message, serial_comm):
+class SerialConnection:
+    """Manage the serial connection."""
+
+    def __init__(self, port, data):
+        self.port = serial.Serial(port)
+        self.data = data
+        self.msg = construct_gpgga(data)
+        self.last_msg = None
+        self.keep_alive()
+
+
+    def send(self, data):
+        """
+        Update the message that we're sending.
+        """
+        self.data = data
+        self.data['timestamp'] = datetime.now().timestamp()*1000
+        self.msg = construct_gpgga(data)
+
+
+    def tick(self):
+        """
+        Check that message has changed, 
+        otherwise update the timestamp
+        and reconstruct the sentence.
+        """
+        if self.msg == self.last_msg:
+            self.data['timestamp'] = datetime.now().timestamp()*1000
+            self.msg = construct_gpgga(self.data)
+
+
+    def keep_alive(self):
+        """
+        Keep the connection alive with
+        valid data.
+        """
+        self.tick()
+        threading.Timer(0.5, self.keep_alive).start()
+        self.port.write(self.msg)
+        self.last_msg = self.msg
+        print(f'Sent: {self.msg}')
+
+
+def construct_gpgga(data):
+    """Construct an NMEA GPGGA sentence from raw data"""
+    utc = ms_to_utc(int(data['timestamp']))
+    lat = float(data['latitude'])
+    long = float(data['longitude'])
+    fix = 1
+    num_sats = 15
+    hdop = 1.5
+    altitude = float(data['altitude'])
+    dsep_geoid = 0
+    dgps_delta_t = ''
+    dgps_station = ''
+
+    if lat < 0:
+        lat_hemi = 'S'
+        lat = -lat
+    else:
+        lat_hemi = 'N'
+
+    if long < 0:
+        long_hemi = 'W'
+        long = -long
+    else:
+        long_hemi = 'E'
+
+    lat_degdmin = f'{int(lat):0>2d}{cvt_deg_to_dmin(lat-int(lat)):.4f}'
+    long_degdmin = f'{int(long):0>3d}{cvt_deg_to_dmin(long-int(long)):.4f}'
+
+    sentence = f'GPGGA,{utc},{lat_degdmin},{lat_hemi},{long_degdmin},{long_hemi},{fix},{num_sats},{hdop:.1f},{altitude:.1f},M,{dsep_geoid:.1f},M,{dgps_delta_t},{dgps_station}'
+    checksum = compute_checksum(sentence)
+    return f'${sentence}*{checksum:X}\r\n'.encode()
+
+
+def compute_checksum(sentence):
+    """Compute checksum of NMEA sentence"""
+    s = 0
+    for c in sentence:
+        s = s ^ ord(c)
+
+    return s
+
+
+def ms_to_utc(ms):
+    """Convert POSIX timestamp to UTC time string"""
+    return datetime.utcfromtimestamp(ms/1000.0).strftime('%H%M%S')
+    
+
+def cvt_deg_to_dmin(deg):
+    """Convert decimal degrees to decimal minutes"""
+    return deg * 60
+
+
+def on_message(ws, message, serial_connection):
     if "gpsdata: " in message:
-        gpsdata = message.replace("gpsdata: ", '').replace("\'", "\"")
-        gpsdatajson = json.loads(gpsdata)
-        nmea = gpsdatajson.get('nmea')
-        if nmea is not None:
-            if serial_comm is None or serial_comm.is_open is False:
-                serial_comm = serial.Serial('/dev/serial0')
-            serial_comm.write(nmea.encode())
-        print("[" + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S::%f')[:-3]) + "] ==> Recieved GPS NMEA Data: " +
-              nmea)
+        data = json.loads(message.lstrip("gpsdata: ").replace("\'", "\""))
+        serial_connection.send(data)
 
 
 def on_error(ws, error):
@@ -50,13 +135,15 @@ def on_open(ws):
 
 
 if __name__ == "__main__":
-    serial_comm = serial.Serial('/dev/serial0')
-    print('Sending gps data to serial port: ' + str(serial_comm.name))
+    mock_data = {'timestamp': datetime.now().timestamp()*1000, 'latitude': 0000.0000, 'longitude': 00000.0000, 'altitude': 0.0}
+    serial_connection = SerialConnection(port='/dev/serial0', data=mock_data)
+    print('Sending GPS data to serial port: ' + serial_connection.port.name) 
+    
     # For Debug:
     # websocket.enableTrace(True)
     print('Starting WebSocket connection to AWS.')
     ws = websocket.WebSocketApp("wss://10eoew3urf.execute-api.us-east-1.amazonaws.com/development",
-                                on_message=lambda ws, msg: on_message(ws, msg, serial_comm),
+                                on_message=lambda ws, msg: on_message(ws, msg, serial_connection),
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
